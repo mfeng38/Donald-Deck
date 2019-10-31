430d0b0e2b9a6cc89a375e12b9eac25c94cec0d3
Chocoerica <ho.ericat@gmail.com>
2019-10-31 14:53:12 -0700

    change file placements so that html,js and css files are in public and ejs files are in views, also changed index.js to render mainmenu at /, and render game at /soloBlackjack

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var app = express();
+//Add database
+const { Pool } = require('pg');
+var pool;
+pool = new Pool({
+    connectionString: process.env.DATABASE_URL//,
+    //ssl:true,
+});
 
-express()
-  .use(express.static(path.join(__dirname, 'views/pages')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/db'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.redirect('mainMenu.html'));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+app.get('/soloBlackjack',(req,res)=> res.render('pages/soloBlackjack'));
---
+++ public/javascript/soloBlackjackAnimations.js
@@ -0,0 +1,3 @@
+// JavaScript soloBlackjack
+// animations and placement of object
+console.log("hello");
