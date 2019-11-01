127e74c3da05877668646271fc5b1c036bafb397
Chocoerica <ho.ericat@gmail.com>
2019-10-31 17:51:17 -0700

    trying to get it to work on cloud

--- index.js
+++ index.js
@@ -2,13 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 var app = express();
-//Add database
-const { Pool } = require('pg');
-var pool;
-pool = new Pool({
-    connectionString: process.env.DATABASE_URL//,
-    //ssl:true,
-});
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
