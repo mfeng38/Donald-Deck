a1977c1dab30f354f5ea2df5067293bb7d40ad70
David Yeung <dya40@sfu.ca>
2019-11-01 17:40:51 -0700

    changed login, will adapt Alyssa/Erica HTML when avaliable

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => res.redirect('mainMenu.html'));
+app.get('/', (req, res) => res.redirect('loginUI.html'));
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
 app.get('/soloBlackjack',(req,res)=> res.render('pages/soloBlackjack'));
