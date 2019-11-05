1daf69b70127c5c54a4bd3a3ba7bda9b56a13c95
David Yeung <dya40@sfu.ca>
2019-11-04 18:36:16 -0800

    outline for BJ game engine

---
+++ public/javascript/blackJackEngine.js
@@ -0,0 +1,21 @@
+// Functions to be implemented //
+
+/* 
+    convert card value to usable value
+        - A -> 11 or 1
+        - 2-9 -> 2-9
+        - J, Q, K -> 10
+*/
+
+/*
+    Hit or stand actions
+        - check if player total <= 21
+            - if > 21, continue to next player/dealer action
+            - if <= 21, continue until player chooses stand
+*/
+
+/*
+    Display values/card images
+        - change img http link to deckofcards API img
+        - change counters innerHTML to corresponding values
+*/
