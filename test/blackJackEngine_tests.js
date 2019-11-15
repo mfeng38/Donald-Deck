829a376664d50b13bc18fbd843278f50209e12d3
David Yeung <dya40@sfu.ca>
2019-11-15 00:01:52 -0800

    test script starteed

---
+++ test/blackJackEngine_tests.js
@@ -0,0 +1,10 @@
+const expect 
+const blackjack = require('../public/javascript/blackJackEngine')
+
+describe('blackjackEngine', () => {
+    it('describe test', () => {
+        // finish test cases
+        const result = blackjack.functionname()
+        expect(result).to.equal(some value)
+    })
+})
