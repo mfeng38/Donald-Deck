3c30c51291d4ea2549d8c00fa33f92f5bd5b5e73
David Yeung <dya40@sfu.ca>
2019-11-18 18:04:21 -0800

    update test file

--- test/blackJackEngine_tests.js
+++ test/blackJackEngine_tests.js
@@ -1,25 +1,24 @@
-const expect = require('chai').expect
-const blackjack = require('../public/javascript/blackJackEngine')
+var chai = require('chai');
+var blackjack = require('../public/javascript/blackJackEngine.js');
 
-describe('blackjackEngine', () => {
-    it('DeckID should return a valid deckID', () => {
-        const result = blackjack.deckID()
-        expect(result).to.be.true
-    })
-    it('gameStart should return true if script ran correctly', () => {
-        const result = blackjack.gameStart()
-        expect(result).to.be.true
-    })
-    it('hit should give current player a card; calculates if player busts', () => {
-        const result = blackjack.hit()
-        expect(result).to.be.true
-    })
-    it('dealer should hit on 16, stay on 17 and follow appropriate actions if bust', () => {
-        const result = blackjack.dealer()
-        expect(result).to.be.true
-    })
-    it('gameStateReset should reset game for new round', () => {
-        const result = blackjack.hit()
-        expect(result).to.be.true
-    })
+
+it('DeckID should return a valid deckID', () => {
+    const result = blackjack.deckID
+    chai.expect(result).to.be.true
 })
+it('gameStart should return true if script ran correctly', () => {
+    const result = blackjack.gameStart
+    chai.expect(result).to.be.true
+})
+it('hit should give current player a card; calculates if player busts', () => {
+    const result = blackjack.hit
+    chai.expect(result).to.be.true
+})
+it('dealer should hit on 16, stay on 17 and follow appropriate actions if bust', () => {
+    const result = blackjack.dealer
+    chai.expect(result).to.be.true
+})
+it('gameStateReset should reset game for new round', () => {
+    const result = blackjack.hit
+    chai.expect(result).to.be.true
+});
