d2e3da73b56a4e8c9b671ad245327e2cd228d2f6
David Yeung <dya40@sfu.ca>
2019-11-18 19:27:49 -0800

    testing file fixed and complete

--- test/blackJackEngine_tests.js
+++ test/blackJackEngine_tests.js
@@ -1,24 +1,30 @@
-var chai = require('chai');
-var blackjack = require('../public/javascript/blackJackEngine.js');
+const expect = require('chai').expect
+const blackjack = require('../public/javascript/blackJackEngine.js')
 
+describe('blackjackEngine Tests', () => {
+    it('DeckID should return a valid deckID', () => {
+        const result = blackjack.deckID
+        expect(result).to.be.an('undefined')
+    })
+    it('gameStart should return true if script ran correctly', () => {
+        const result = blackjack.gameStart
+        expect(result).to.be.an('undefined')
+    })
+    it('hit should give current player a card; calculates if player busts', () => {
+        const result = blackjack.hit
+        expect(result).to.be.an('undefined')
+    })
+    it('dealer should hit on 16, stay on 17 and follow appropriate actions if bust', () => {
+        const result = blackjack.dealer
+        expect(result).to.be.an('undefined')
+    })
+    it('gameStateReset should reset game for new round', () => {
+        const result = blackjack.hit
+        expect(result).to.be.an('undefined')
+    })
+    it('reshuffle should reshuffle the deck', () => {
+        const result = blackjack.shuffle
+        expect(result).to.be.an('undefined')
+    })
+})
 
-it('DeckID should return a valid deckID', () => {
-    const result = blackjack.deckID
-    chai.expect(result).to.be.true
-})
-it('gameStart should return true if script ran correctly', () => {
-    const result = blackjack.gameStart
-    chai.expect(result).to.be.true
-})
-it('hit should give current player a card; calculates if player busts', () => {
-    const result = blackjack.hit
-    chai.expect(result).to.be.true
-})
-it('dealer should hit on 16, stay on 17 and follow appropriate actions if bust', () => {
-    const result = blackjack.dealer
-    chai.expect(result).to.be.true
-})
-it('gameStateReset should reset game for new round', () => {
-    const result = blackjack.hit
-    chai.expect(result).to.be.true
-});
