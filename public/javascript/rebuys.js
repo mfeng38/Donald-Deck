ad46100ebef24e2abe7704ded6a0f28611cf9b89
Chocoerica <ho.ericat@gmail.com>
2019-11-26 00:45:21 -0800

    rebuy appearance, inner workings still not done

--- public/javascript/rebuys.js
+++ public/javascript/rebuys.js
@@ -1,14 +1,16 @@
 //js for rebuys (client side)
 //rebuys
+var div = document.getElementById("rebuyPrompt");
+div.display = "none";
 var numcredits = document.getElementById('creditnumber');
 if (numcredits < 500) {
-    document.getElementById('rebuys').style.visibility = "visible";
-    document.getElementById('rebuyPrompt').style.visibility = "visible";
+    div.display = "block";
 }
 
-document.getElementById('rebuy').addEventListener('click', function () {
+/*document.getElementById('rebuy').addEventListener('click', function () {
     //send post request to index.js
     var http = new XMLHttpRequest();
     http.open("POST", "/rebuy", true);
     http.send();
 });
+*/
