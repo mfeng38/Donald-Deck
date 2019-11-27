7518c30db503a992e537d953c0a095b2cc47412e
Alyssa <anicolso@sfu.ca>
2019-11-26 17:13:32 -0800

    Class Test

--- public/javascript/blackJackEngineEvents.js
+++ public/javascript/blackJackEngineEvents.js
@@ -1,4 +1,4 @@
-document.getElementById('startround').addEventListener('click', function () {
+document.getElementsByClassName('startround').addEventListener('click', function () {
     var bet = document.getElementById("CurrentBet").value;
     if (Number.isNaN(bet) || bet==0||bet==undefined){
         return;
--- public/javascript/soloBlackjackAnimations.js
+++ public/javascript/soloBlackjackAnimations.js
@@ -130,7 +130,7 @@ document.getElementById('start').addEventListener('click', function () {
 
 });
 
-document.getElementById('startround').addEventListener('click', function () {
+document.getElementsByClassName('startround').addEventListener('click', function () {
     var bet = document.getElementById("CurrentBet").value;
     if (Number.isNaN(bet) || bet==0||bet==undefined){
         return;
