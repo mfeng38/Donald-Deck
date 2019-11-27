83225b1a5be083cfaad545cb15fac419cb00c983
Alyssa <anicolso@sfu.ca>
2019-11-26 17:46:48 -0800

    revert

--- public/javascript/soloBlackjackAnimations.js
+++ public/javascript/soloBlackjackAnimations.js
@@ -167,12 +167,14 @@ document.getElementById('changeBet').addEventListener('click', function () {
 });
 
 document.getElementById('playAgain').addEventListener('click', function () {
+  /*
     document.getElementById("startround").style.visibility = "visible";
     document.getElementById("backofcard").style.visibility = "visible";
     document.getElementById("Dealer").style.visibility = "hidden";
     document.getElementById("Player").style.visibility = "hidden";
     document.getElementById("dealerCounter").style.visibility = "hidden";
     document.getElementById("playerCounter").style.visibility = "hidden";
+    */
 
 });
 
