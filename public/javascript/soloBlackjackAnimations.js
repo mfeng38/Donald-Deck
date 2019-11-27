66a656c0e93183d5420834b0a2b9258d27642c77
Alyssa <anicolso@sfu.ca>
2019-11-26 17:21:31 -0800

    play again rework

--- public/javascript/soloBlackjackAnimations.js
+++ public/javascript/soloBlackjackAnimations.js
@@ -167,13 +167,22 @@ document.getElementById('changeBet').addEventListener('click', function () {
 });
 
 document.getElementById('playAgain').addEventListener('click', function () {
-    document.getElementById("startround").style.visibility = "visible";
-    document.getElementById("backofcard").style.visibility = "visible";
-    document.getElementById("Dealer").style.visibility = "hidden";
-    document.getElementById("Player").style.visibility = "hidden";
-    document.getElementById("dealerCounter").style.visibility = "hidden";
-    document.getElementById("playerCounter").style.visibility = "hidden";
+    var bet = document.getElementById("CurrentBet").value;
+    console.log(bet);
+    document.getElementById('startround').style.visibility = 'hidden';
+    document.getElementById("CurrentBet").style.visibility = "hidden";
 
+    document.getElementById("bet").innerHTML = `Bet: ${bet}`;
+    document.getElementById("bet").style.visibility = "visible";
+
+    document.getElementById("Player").style.visibility = "visible";
+    document.getElementById("Dealer").style.visibility = "visible";
+    document.getElementById("playerCounter").style.visibility = "visible";
+    document.getElementById("dealerCounter").style.visibility = "visible";
+    document.getElementById("backofcard").style.visibility = "hidden";
+    document.getElementById("hit").style.visibility = "visible";
+    document.getElementById("stay").style.visibility = "visible";
+    }
 });
 
 function hideBetButtons() {
