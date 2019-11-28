873b3bfdac9a030416b9658d4d9dc707ee081091
Alyssa <anicolso@sfu.ca>
2019-11-27 20:39:39 -0800

    multiplayerBlackjack + animations

---
+++ public/javascript/multiBlackjackAnimations.js
@@ -0,0 +1,14 @@
+//Readying up
+document.getElementById('readyUp').addEventListener('click', function () {
+    document.getElementById("CurrentBet").style.visibility= "visible";
+    document.getElementById("readyUp").style.display = "none";
+    document.getElementById("startround").style.visibility = "visible";
+    showBetButtons();
+
+});
+
+
+//Fill this boi with leaderboard stuff
+document.getElementById('viewLeaderboard').addEventListener('click', function() {
+
+});
