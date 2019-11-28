c0aca72f35be89f2cf4da75ff035c04144ea0f17
Alyssa <anicolso@sfu.ca>
2019-11-27 21:00:22 -0800

    multiplayer animation + CSS changes
    multiplayer animation + CSS changes
    
    multiplayer animation + CSS changes
    
    multiplayer animation + CSS changes
    
    multiplayer animation + CSS changes
    
    multiplayer animation + CSS changes
    
    what is happening.jpg
    
    what-is-happening.jpg
    
    what-is-happening.jpg
    
    what-is-happening.jpg
    
    shared animations
    
    shared animations
    
    card rework
    
    shared animations
    
    move card
    
    move card
    
    move card
    
    move card

--- public/javascript/multiBlackjackAnimations.js
+++ public/javascript/multiBlackjackAnimations.js
@@ -1,3 +1,4 @@
+
 //Readying up
 document.getElementById('readyUp').addEventListener('click', function () {
     document.getElementById("roomID").style.visibility= "hidden";
@@ -5,7 +6,6 @@ document.getElementById('readyUp').addEventListener('click', function () {
     document.getElementById("CurrentBet").style.visibility= "visible";
     document.getElementById("startMatch").style.visibility = "visible";
     showBetButtons();
-
 });
 
 document.getElementById('startMatch').addEventListener('click', function () {
@@ -17,6 +17,7 @@ document.getElementById('startMatch').addEventListener('click', function () {
         console.log(bet);
         document.getElementById('startMatch').style.visibility = 'hidden';
         document.getElementById("CurrentBet").style.visibility = "hidden";
+        document.getElementById("multicontainer").style.visibility = "hidden";
 
         document.getElementById("bet").innerHTML = `Bet: ${bet}`;
         document.getElementById("bet").style.visibility = "visible";
@@ -25,15 +26,44 @@ document.getElementById('startMatch').addEventListener('click', function () {
         document.getElementById("Dealer").style.visibility = "visible";
         document.getElementById("playerCounter").style.visibility = "visible";
         document.getElementById("dealerCounter").style.visibility = "visible";
-        document.getElementById("backofcard").style.visibility = "hidden";
         document.getElementById("hit").style.visibility = "visible";
         document.getElementById("stay").style.visibility = "visible";
         document.getElementById("viewLeaderboard").style.visibility = "visible";
         hideBetButtons();
     }
+    gameStart();
+});
+
+document.getElementById('changeBet').addEventListener('click', function () {
+    document.getElementById("Dealer").style.visibility = "hidden";
+    document.getElementById("Player").style.visibility = "hidden";
+    document.getElementById("dealerCounter").style.visibility = "hidden";
+    document.getElementById("playerCounter").style.visibility = "hidden";
+    document.getElementById("bet").style.visibility = "hidden";
+    document.getElementById('winloss').style.visibility = "hidden";
+    document.getElementById("changeBet").style.visibility = "hidden";
+    document.getElementById("playAgain").style.visibility = "hidden";
+
+    document.getElementById("startMatch").style.visibility = "visible";
+    document.getElementById("CurrentBet").style.visibility = "visible";
+    showBetButtons();
+});
+
+document.getElementById('playAgain').addEventListener('click', function () {
+    document.getElementById('winloss').style.visibility = "hidden";
+    document.getElementById("changeBet").style.visibility = "hidden";
+    document.getElementById("playAgain").style.visibility = "hidden";
+
+    document.getElementById("hit").style.visibility = "visible";
+    document.getElementById("stay").style.visibility = "visible";
+    gameStart();
 });
 
 //Fill this boi with leaderboard stuff
 document.getElementById('viewLeaderboard').addEventListener('click', function() {
 
 });
+
+//////////////////////////////////
+//    Modified file end here    //
+//////////////////////////////////
--- public/javascript/soloBlackjackAnimations.js
+++ public/javascript/soloBlackjackAnimations.js
@@ -1,125 +1,5 @@
 // JavaScript soloBlackjack
 // animations and placement of object
-console.log(document.getElementById(CurrentBet.value));
-
-document.getElementById('Add5').addEventListener('click', function () {
-    var currbet = document.getElementById("CurrentBet").value;
-    console.log("click:", parseInt(currbet));
-    var numcoins = parseInt(currbet);
-    if (Number.isNaN(numcoins)) {
-        numcoins = 5;
-    }
-    else {
-        numcoins = numcoins + 5;
-    }
-    document.getElementById("CurrentBet").innerHTML = "Current Bet: " + numcoins;
-    document.getElementById("CurrentBet").value = numcoins;
-});
-document.getElementById('Add10').addEventListener('click', function () {
-    var currbet = document.getElementById("CurrentBet").value;
-    console.log("click:", parseInt(currbet));
-    var numcoins = parseInt(currbet);
-    if (Number.isNaN(numcoins)) {
-        numcoins = 10;
-        console.log(numcoins);
-    }
-    else {
-        numcoins = numcoins + 10;
-    }
-    document.getElementById("CurrentBet").innerHTML = "Current Bet: " + numcoins;
-    document.getElementById("CurrentBet").value = numcoins;
-});
-document.getElementById('Add25').addEventListener('click', function () {
-    var currbet = document.getElementById("CurrentBet").value;
-    console.log("click:", parseInt(currbet));
-    var numcoins = parseInt(currbet);
-    if (Number.isNaN(numcoins)) {
-        numcoins = 25;
-    }
-    else {
-        numcoins = numcoins + 25;
-    }
-    document.getElementById("CurrentBet").innerHTML = "Current Bet: " + numcoins;
-    document.getElementById("CurrentBet").value = numcoins;
-});
-document.getElementById('Add100').addEventListener('click', function () {
-    var currbet = document.getElementById("CurrentBet").value;
-    console.log("click:", parseInt(currbet));
-    var numcoins = parseInt(currbet);
-    if (Number.isNaN(numcoins)) {
-        numcoins = 100;
-    }
-    else {
-        numcoins = numcoins + 100;
-    }
-    document.getElementById("CurrentBet").innerHTML = "Current Bet: " + numcoins;
-    document.getElementById("CurrentBet").value = numcoins;
-});
-
-document.getElementById('Sub5').addEventListener('click', function () {
-    var currbet = document.getElementById("CurrentBet").value;
-    console.log("click: -5", parseInt(currbet));
-    var numcoins = parseInt(currbet);
-    if (Number.isNaN(numcoins)) {
-        numcoins = 0;
-    }
-    else {
-        if (numcoins >= 5) {
-            numcoins = numcoins - 5;
-        }
-    }
-    document.getElementById("CurrentBet").innerHTML = "Current Bet: " + numcoins;
-    document.getElementById("CurrentBet").value = numcoins;
-});
-
-document.getElementById('Sub10').addEventListener('click', function () {
-    var currbet = document.getElementById("CurrentBet").value;
-    console.log("click: -10", parseInt(currbet));
-    var numcoins = parseInt(currbet);
-    if (Number.isNaN(numcoins)) {
-        numcoins = 0;
-    }
-    else {
-        if (numcoins >= 10) {
-            numcoins = numcoins - 10;
-        }
-    }
-    document.getElementById("CurrentBet").innerHTML = "Current Bet: " + numcoins;
-    document.getElementById("CurrentBet").value = numcoins;
-});
-
-document.getElementById('Sub25').addEventListener('click', function () {
-    var currbet = document.getElementById("CurrentBet").value;
-    console.log("click: -25", parseInt(currbet));
-    var numcoins = parseInt(currbet);
-    if (Number.isNaN(numcoins)) {
-        numcoins = 0;
-    }
-    else {
-        if (numcoins >= 25) {
-            numcoins = numcoins - 25;
-        }
-    }
-    document.getElementById("CurrentBet").innerHTML = "Current Bet: " + numcoins;
-    document.getElementById("CurrentBet").value = numcoins;
-});
-
-document.getElementById('Sub100').addEventListener('click', function () {
-    var currbet = document.getElementById("CurrentBet").value;
-    console.log("click: -100");
-    var numcoins = parseInt(currbet);
-    if (Number.isNaN(numcoins)) {
-        numcoins = 0;
-    }
-    else {
-        if (numcoins >= 100) {
-            numcoins = numcoins - 100;
-        }
-    }
-    document.getElementById("CurrentBet").innerHTML = "Current Bet: " + numcoins;
-    document.getElementById("CurrentBet").value = numcoins;
-});
-
 
 document.getElementById('start').addEventListener('click', function () {
     //to start the game:
@@ -150,49 +30,32 @@ document.getElementById('startround').addEventListener('click', function () {
         document.getElementById("backofcard").style.visibility = "hidden";
         document.getElementById("hit").style.visibility = "visible";
         document.getElementById("stay").style.visibility = "visible";
-        //document.getElementById("viewLeaderboard").style.visibility = "visible";
         hideBetButtons();
     }
 });
 
 document.getElementById('changeBet').addEventListener('click', function () {
     showBetButtons();
-    document.getElementById("startround").style.visibility = "visible";
-    document.getElementById("backofcard").style.visibility = "visible";
     document.getElementById("Dealer").style.visibility = "hidden";
     document.getElementById("Player").style.visibility = "hidden";
     document.getElementById("dealerCounter").style.visibility = "hidden";
     document.getElementById("playerCounter").style.visibility = "hidden";
     document.getElementById("bet").style.visibility = "hidden";
+    document.getElementById('winloss').style.visibility = "hidden";
+    document.getElementById("changeBet").style.visibility = "hidden";
+    document.getElementById("playAgain").style.visibility = "hidden";
+
     document.getElementById("CurrentBet").style.visibility = "visible";
+    document.getElementById("startround").style.visibility = "visible";
+    document.getElementById("backofcard").style.visibility = "visible";
 });
 
 document.getElementById('playAgain').addEventListener('click', function () {
+    document.getElementById('winloss').style.visibility = "hidden";
+    document.getElementById("changeBet").style.visibility = "hidden";
+    document.getElementById("playAgain").style.visibility = "hidden";
+
     document.getElementById("hit").style.visibility = "visible";
     document.getElementById("stay").style.visibility = "visible";
     gameStart();
-
 });
-
-function hideBetButtons() {
-    document.getElementById("Add5").style.visibility = "hidden";
-    document.getElementById("Add10").style.visibility = "hidden";
-    document.getElementById("Add25").style.visibility = "hidden";
-    document.getElementById("Add100").style.visibility = "hidden";
-    document.getElementById("Sub5").style.visibility = "hidden";
-    document.getElementById("Sub10").style.visibility = "hidden";
-    document.getElementById("Sub25").style.visibility = "hidden";
-    document.getElementById("Sub100").style.visibility = "hidden";
-    document.getElementById("CurrentBet").style.visibility = "hidden";
-}
-function showBetButtons() {
-    document.getElementById("Add5").style.visibility = "visible";
-    document.getElementById("Add10").style.visibility = "visible";
-    document.getElementById("Add25").style.visibility = "visible";
-    document.getElementById("Add100").style.visibility = "visible";
-    document.getElementById("Sub5").style.visibility = "visible";
-    document.getElementById("Sub10").style.visibility = "visible";
-    document.getElementById("Sub25").style.visibility = "visible";
-    document.getElementById("Sub100").style.visibility = "visible";
-    document.getElementById("CurrentBet").style.visibility = "visible";
-}
