bbe6377393e81c0c2bc1bc47b87de48a3dcb88bc
Alyssa <anicolso@sfu.ca>
2019-12-02 03:58:59 -0800

    gonnnaaa fiiiite this leaderboard

--- public/javascript/multiBlackjackAnimations.js
+++ public/javascript/multiBlackjackAnimations.js
@@ -51,7 +51,6 @@ document.getElementById('changeBet').addEventListener('click', function () {
     document.getElementById("playAgain").style.visibility = "hidden";
     document.getElementById("viewLeaderboard").style.visibility = "hidden";
 
-    document.getElementById("multicontainer").style.visibility = "visible";
     document.getElementById("startMatch").style.visibility = "visible";
     document.getElementById("CurrentBet").style.visibility = "visible";
     showBetButtons();
