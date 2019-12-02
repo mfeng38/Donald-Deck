36c931070c9ac0976167c5fc7b1a29a599f29284
David Yeung <dya40@sfu.ca>
2019-12-02 14:56:55 -0800

    big fixes

--- databaseCMDs.sql
+++ databaseCMDs.sql
@@ -22,15 +22,15 @@ CREATE TABLE users(
 
 CREATE FUNCTION creditReset() RETURNS TRIGGER AS $$
 BEGIN
-    IF users.credits <= 0 THEN
-        UPDATE users
-        SET credits = 2000,
-            rebuys = rebuys + 1
-        WHERE credits <= 0;
-    END IF;
+    UPDATE users
+    SET credits = 2000,
+        rebuys = rebuys + 1
+    WHERE credits <= 0;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER creditResetTrigger
 AFTER UPDATE ON users
-FOR EACH ROW EXECUTE PROCEDURE creditReset();
+FOR EACH ROW
+WHEN (OLD.credits <= 0)
+EXECUTE PROCEDURE creditReset();
