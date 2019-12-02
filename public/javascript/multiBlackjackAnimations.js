e395871551ea20be65679e2df49ca085d400c370
Alyssa <anicolso@sfu.ca>
2019-12-02 04:01:19 -0800

    gonnnaaa fiiiite this leaderboard

--- public/javascript/multiBlackjackAnimations.js
+++ public/javascript/multiBlackjackAnimations.js
@@ -68,16 +68,16 @@ document.getElementById('playAgain').addEventListener('click', function () {
 
 document.getElementById('viewLeaderboard').addEventListener('click', function(){
     document.getElementById('leaderboardContainer').style.visibility = "visible";
-    document.getElementById("hit").style.visibility = "hidden";
-    document.getElementById("stay").style.visibility = "hidden";
+    //document.getElementById("hit").style.visibility = "hidden";
+    //document.getElementById("stay").style.visibility = "hidden";
     document.getElementById('winloss').style.visibility = "hidden";
 
 });
 
 document.getElementById('closeLeaderboard').addEventListener('click', function(){
     document.getElementById('leaderboardContainer').style.visibility = "hidden";
-    document.getElementById("hit").style.visibility = "visible";
-    document.getElementById("stay").style.visibility = "visible";
+    //document.getElementById("hit").style.visibility = "visible";
+    //document.getElementById("stay").style.visibility = "visible";
 });
 
 //////////////////////////////////
