0a00685aa644d9621593a62e320453b022c6a031
David Yeung <dya40@sfu.ca>
2019-12-02 14:59:27 -0800

    bug fixes

--- databaseCMDs.sql
+++ databaseCMDs.sql
@@ -13,9 +13,24 @@ CREATE TABLE users(
 /* Create Rooms table (do in iteration 2/3)*/
 
 /* Table for User cards while playing --> should we put credits here, or is it better to access two tables*/
-CREATE TABLE games(
-	tablenum ???
-	username TEXT,
-	credits INTEGER, 
-	currhand TEXT
-);
+-- CREATE TABLE games(
+-- 	tablenum ???
+-- 	username TEXT,
+-- 	credits INTEGER, 
+-- 	currhand TEXT
+-- );
+
+-- CREATE FUNCTION creditReset() RETURNS TRIGGER AS $$
+-- BEGIN
+--     UPDATE users
+--     SET credits = 2000,
+--         rebuys = rebuys + 1
+--     WHERE credits <= 0;
+-- END;
+-- $$ LANGUAGE plpgsql;
+
+-- CREATE TRIGGER creditResetTrigger
+-- AFTER UPDATE ON users
+-- FOR EACH ROW
+-- WHEN (OLD.credits <= 0)
+-- EXECUTE PROCEDURE creditReset();
